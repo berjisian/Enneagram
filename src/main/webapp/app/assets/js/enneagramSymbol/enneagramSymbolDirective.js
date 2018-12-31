d784c0631fec36db7c6b533f7f932c95a770e0f9
MHesam <mohammadhesam.berjisian@gmail.com>
2018-12-31 21:43:49 +0330

    pre-explanation, phase 2

--- src/main/webapp/app/assets/js/enneagramSymbol/enneagramSymbolDirective.js
+++ src/main/webapp/app/assets/js/enneagramSymbol/enneagramSymbolDirective.js
@@ -3,12 +3,29 @@ angular.module("enneagramSymbol", []).directive('enneagramSymbol', function() {
         restrict : 'E',
         templateUrl : 'app/assets/js/enneagramSymbol/enneagramSymbol.html',
         scope : {
-
+            api: '='
         },
 
         controller : function($scope) {
 
-            const Run = function () {};
+            $scope.Data = {
+                groups: []
+            };
+
+            $scope.Func = {
+                prepareGroups: function () {
+                    for (let i = 1; i < 10; i++)
+                        $scope.Data.groups.push(i);
+                },
+                onGroupSelect: function (groupNum) {
+                    $scope.api.selectedGroup = groupNum;
+                    $scope.api.mode = "selected"
+                }
+            };
+
+            const Run = function () {
+                $scope.Func.prepareGroups();
+            };
 
             Run()
         },
--- src/main/webapp/app/modules/detection/explanationModal/explanationModalCtrl.js
+++ src/main/webapp/app/modules/detection/explanationModal/explanationModalCtrl.js
@@ -1,10 +1,29 @@
 angular.module('detectionModule').controller('explanationModalCtrl', function ($scope, $uibModalInstance) {
 
-    $scope.Data = {};
+    $scope.Data = {
+        mode: "none",
+        selectedGroup: ""
+    };
 
     $scope.Func = {
         onCancelClick: function () {
             $uibModalInstance.close(false);
         }
     };
+
+    $scope.Controller = {
+        symbolDirective: {
+            mode: "none",
+            selectedGroup: ""
+        }
+    };
+
+    const Run = function () {
+        $scope.$watch("Controller.symbolDirective.selectedGroup", function () {
+            $scope.Data.mode = $scope.Controller.symbolDirective.mode;
+            $scope.Data.selectedGroup = $scope.Controller.symbolDirective.selectedGroup;
+        }, true);
+    };
+
+    Run()
 });
