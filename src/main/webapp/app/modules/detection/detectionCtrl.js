1c1b5676a30bfcde3cb27b970596a00eba1f4511
MHesam <mohammadhesam.berjisian@gmail.com>
2019-05-03 18:42:43 +0430

    Changed the test!

--- src/main/webapp/app/modules/detection/detectionCtrl.js
+++ src/main/webapp/app/modules/detection/detectionCtrl.js
@@ -4,10 +4,10 @@ angular.module('detectionModule').controller('detectionCtrl', function ($scope, 
         mode: "none",
         tempProbableGroups: [],
         tempPreferredGroups: [],
+        groups: [1, 2, 3, 4, 5, 6, 7, 8, 9],
         probableGroups: [0, 0, 0, 0, 0, 0, 0, 0, 0],
         preferredGroups: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        tempResultGroups: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        resultGroups: [],
+        resultGroups: [0, 0, 0, 0, 0, 0, 0, 0, 0],
         mbtiGroups: ["INTJ", "INTP", "INFJ", "INFP", "ISTJ", "ISTP", "ISFJ", "ISFP",
                     "ENTJ", "ENTP", "ENFJ", "ENFP", "ESTJ", "ESTP", "ESFJ", "ESFP"],
         approvedMbtiGroup: "",
@@ -21,7 +21,81 @@ angular.module('detectionModule').controller('detectionCtrl', function ($scope, 
         page: {
             size: 15,
             num: 0
-        }
+        },
+        allQuestions: "خیال‌باف و رؤیاپرداز هستم\n" +
+            "مرد عمل و واقع‌بین هستم\n" +
+            "معمولاً مشکلی با ایستادن در روی دیگران ندارم\n" +
+            "سعی می‌کنم حتی‌الامکان با بقیه جروبحث نکنم\n" +
+            "به طور معمول مردم‌دار، جذاب و بلندپرواز هستم\n" +
+            "به طور معمول خشک و رسمی و آرمان‌گرا هستم\n" +
+            "معمولاً اجازه نمی‌دهم چیزی حواسم را پرت کند\n" +
+            "سعی می‌کنم در لحظه زندگی کنم و لذت ببرم\n" +
+            "خون‌گرم هستم و از پیدا کردن دوست‌های تازه خوشحال می‌شوم\n" +
+            "با دیگران زیاد گرم نمی‌گیرم\n" +
+            "به راحتی مضطرب می‌شوم\n" +
+            "چندان پیش نمی‌آید که خونسردی‌ام را از دست بدهم\n" +
+            "دست به هرکاری می‌زنم تا گلیم خودم را از آب بکشم\n" +
+            "به هیچ وجه حاضر نیستم اصول و عقاید خودم را زیر پا بگذارم\n" +
+            "نیاز دارم علاقه‌ام را به سایرین ابراز کنم\n" +
+            "ترجیح می‌دهم فاصله‌ام را با دیگران حفظ کنم\n" +
+            "وقتی کار جدیدی را شروع می‌کنم دوست دارم بدانم چه نفعی برایم دارد\n" +
+            "اگر قرار است کار جدیدی را شروع کنم، دوست دارم برایم لذت‌بخش باشد\n" +
+            "اکثر اوقات توجهم معطوف به خودم است\n" +
+            "اکثر اوقات توجهم معطوف به سایرین است\n" +
+            "نقطه‌ی قوت من آگاهی و دانشم است\n" +
+            "نقطه‌ی قوت من قدرت و قاطعیتم است\n" +
+            "چندان به توانایی‌های خودم اطمینان ندارم\n" +
+            "اعتماد به نفس کامل دارم\n" +
+            "روابطم با سایرین برایم مهم‌تر از رسیدن به اهدافم است\n" +
+            "رسیدن به اهدافم برایم مهم‌تر از روابطم با سایرین است\n" +
+            "معمولاْ جرئت نمی‌کنم نظرات خودم را بیان کنم\n" +
+            "نظراتم را به راحتی مطرح می‌کنم\n" +
+            "برایم دشوار است که از راهکارهای دیگر چشم بپوشم و فقط به یک روش خاص عمل کنم\n" +
+            "برایم دشوار است که آسان‌گیر و انعطاف‌پذیر باشم\n" +
+            "معمولاً دودل‌ام و زیاد تردید می‌کنم\n" +
+            "بی‌پروا و اهل خطرکردن‌ام\n" +
+            "همیشه سعی کرده‌ام خیلی با دیگران گرم نگیرم و این موضوع مشکلات زیادی به وجود آورده\n" +
+            "همیشه دوست داشته‌ام دیگران به من متکی باشند و این موضوع برایم مشکل‌آفرین شده\n" +
+            "راحت می‌توانم احساساتم را کنار بگذارم و کاری را انجام بدهم\n" +
+            "همیشه باید اول با احساساتم کنار بیایم تا بعد بتوانم کاری را شروع کنم\n" +
+            "معمولاْ آدم محتاطی هستم و کارهایم روی حساب و کتاب است\n" +
+            "معمولاً ماجراجو و اهل خطر کردن هستم\n" +
+            "از معاشرت با دیگران لذت می‌برم و سعی می‌کنم در موقع نیاز کمک‌حالشان باشم\n" +
+            "آدم توداری هستم و جز در موقع ضرورت کاری به کار بقیه ندارم\n" +
+            "خیلی وقت ها احساس می‌کنم که باید مثل کوه محکم باشم\n" +
+            "خیلی وقت ها احساس می‌کنم که باید دقیق و بی‌عیب‌و‌نقص باشم\n" +
+            "دوست دارم به هر قیمتی که شده استقلال خودم را حفظ کنم\n" +
+            "دوست دارم به هر قیمتی که شده آرامش روحی و روانی خودم را حفظ کنم\n" +
+            "بیش از حد بدبین و شکاک هستم\n" +
+            "بیش از حد خوش‌قلب و احساساتی هستم\n" +
+            "اغلب نگرانم که مبادا فرصت‌های بهتری وجود داشته باشند و من آن‌ها را از دست بدهم\n" +
+            "اغلب نگرانم که اگر غافل بشوم، کسی از وضعیتم سوء استفاده بکند\n" +
+            "به قدری سعی می‌کنم خاص و منحصربفرد باشم که مردم از دستم ذله می‌شوند\n" +
+            "زیاد برای بقیه تعیین تکلیف می‌کنم و مردم از دستم شاکی هستند\n" +
+            "وقتی مشکلی پیش می‌آید، راحت می‌توانم آن را نادیده بگیرم\n" +
+            "وقتی مشکلی پیش می‌آید، سرم را با کار جالبی گرم می‌کنم\n" +
+            "به دوستانم اعتماد دارم و آن‌ها هم می‌دانند که می‌توانند روی من حساب باز کنند\n" +
+            "به دیگران تکیه نمی‌کنم و کارهایم را خودم انجام می‌دهم\n" +
+            "آدم حواس‌پرتی هستم و همیشه فکرم جای دیگری است\n" +
+            "گاه و بی‌گاه در لاک خودم فرو می‌روم و گرفته و ترشرو می‌شوم\n" +
+            "دوست دارم بقیه را به چالش بکشم و به تحرک وادار کنم\n" +
+            "دوست دارم در موقع نیاز به بقیه کمک کنم و به آن‌ها آرامش بدهم\n" +
+            "خون‌گرم و اجتماعی هستم\n" +
+            "کوشا، جدی و منضبط هستم\n" +
+            "خجالت می‌کشم که مهارت‌هایم را در حضور سایرین به نمایش بگذارم\n" +
+            "دوست دارم مهارت‌ها و توانایی‌هایم را به بقیه نشان بدهم\n" +
+            "برای من دنبال کردن علایق شخصی مهم‌تر از داشتن ثبات و امنیت است\n" +
+            "برای من ثبات و امنیت مهم‌تر از دنبال کردن علایق شخصی است\n" +
+            "وقتی با دیگران اختلاف نظر پیدا می‌کنم، کوتاه می‌آیم\n" +
+            "وقتی با دیگران اختلاف نظر پیدا می‌کنم، به هیچ وجه پا پس نمی‌کشم\n" +
+            "معمولاً راحت کوتاه می‌آیم و به ساز دیگران می‌رقصم\n" +
+            "به خواسته‌ی بقیه تن در نمی‌دهم و کاری را که به نظرم درست است، انجام می‌دهم\n" +
+            "حس شوخ‌طبعی بی‌نظیرم و روحیه‌ی مثبتم مایه‌ی تحسین بقیه است\n" +
+            "حضورم به بقیه آرامش می‌دهد و می‌دانند که می‌توانند روی کمک من حساب باز کنند\n" +
+            "راحت می‌توانم در دل مردم جا باز کنم و این مهارت خیلی جاها به دادم رسیده\n" +
+            "با وجود این که مهارت‌های اجتماعی چندانی ندارم، توانسته‌ام از عهده‌ی کارهایم بربیایم",
+        answerKey: [4, 6, 8, 9, 3, 1, 5, 7, 2, 4, 6, 9, 8, 1, 2, 5, 3, 7, 4, 9, 5, 8, 6, 1, 2, 3, 4, 7, 5, 1, 6, 8, 9, 2,
+            3, 4, 6, 7, 2, 1, 8, 3, 5, 9, 6, 2, 7, 8, 4, 1, 9, 7, 6, 3, 5, 4, 8, 2, 7, 1, 9, 3, 5, 6, 4, 8, 9, 1, 7, 2, 3, 5]
     };
 
     $scope.Func = {
@@ -34,19 +108,21 @@ angular.module('detectionModule').controller('detectionCtrl', function ($scope, 
             $scope.Data.tempPreferredGroups = angular.copy($scope.Data.tempProbableGroups);
         },
         prepareQuestions: function () {
-            detectionSrvc.getQuestions().done(function (questionsString) {
-                let questions = questionsString.split("\n");
-                let i = 0;
-                _.each(questions, function (question) {
-                    $scope.Data.questions.push({
-                        num: i,
-                        value: (i+1) + ". " + question,
-                        answer: 0
-                    });
-                    i++;
+            let questions = $scope.Data.allQuestions.split("\n");
+            for (let i = 0; i < (questions.length / 2); i++) {
+                $scope.Data.questions.push({
+                    num: i,
+                   first: {
+                       phrase: questions[2 * i],
+                       value: $scope.Data.answerKey[2 * i]
+                   },
+                   second: {
+                       phrase: questions[2 * i + 1],
+                       value: $scope.Data.answerKey[2 * i + 1]
+                   },
+                    answer: null
                 });
-                $scope.Func.prepareCurrentQuestions();
-            });
+            }
         },
         onOpenExplanationClick: function () {
             $uibModal.open({
@@ -77,53 +153,34 @@ angular.module('detectionModule').controller('detectionCtrl', function ($scope, 
             else
                 $scope.Data.tempPreferredGroups[group - 1].active = !$scope.Data.tempPreferredGroups[group - 1].active;
         },
-        onAnswerSelect: function (answer, questionNum) {
-            for (let i = 0; i < $scope.Data.questions.length; i++) {
-                if (questionNum === $scope.Data.questions[i].num)
-                    $scope.Data.questions[i].answer = answer;
-            }
-            for (let i = 0; i < $scope.Data.currentQuestions.length; i++) {
-                if (questionNum === $scope.Data.currentQuestions[i].num)
-                    $scope.Data.currentQuestions[i].answer = answer;
-            }
-        },
-        prepareCurrentQuestions: function () {
-            let answer = $scope.Func.isAllAnswered();
-            if (answer === "answered") {
-                $scope.Data.goingNextPage = false;
-                let start = $scope.Data.page.num * $scope.Data.page.size;
-                $scope.Data.currentQuestions = angular.copy($scope.Data.questions.slice(start, start + $scope.Data.page.size));
-                $scope.Data.page.num++;
-                if ($scope.Data.page.num === ($scope.Data.questions.length / $scope.Data.page.size))
-                    $scope.Data.lastPage = true;
-                $scope.Func.scrollToTop();
-            } else {
-                $scope.Data.goingNextPage = true;
-                $("html, body").animate({scrollTop: $('#question-' + answer.num).offset().top }, 500);
+        onAnswerSelect: function (question, value) {
+            if (question.answer !== value) {
+                if (question.answer)
+                    $scope.Data.resultGroups[question.answer - 1]--;
+                question.answer = value;
+                $scope.Data.resultGroups[question.answer - 1]++;
             }
         },
         isAllAnswered: function () {
             let answer = "";
-            _.each($scope.Data.currentQuestions, function (question) {
-                if (!answer && question.answer === 0)
+            _.each($scope.Data.questions, function (question) {
+                if (!answer && !question.answer)
                     answer = question;
             });
-            if (!answer || !$scope.Data.currentQuestions.length)
+            if (!answer || !$scope.Data.questions.length)
                 answer = "answered";
-            return "answered";
+            return answer;
         },
         calculateTestResult: function () {
-            _.each($scope.Data.questions, function (question) {
-                $scope.Data.tempResultGroups[question.num % 9] += question.answer;
-            });
-            for (let i = 0; i < 9; i++) {
-                $scope.Data.resultGroups[i] = {
-                    value: i+1,
-                    result: Math.floor($scope.Data.tempResultGroups[i] / 15) - 3
-                }
+            let answer = $scope.Func.isAllAnswered();
+            if (answer === "answered") {
+                $scope.Data.showAnswer = false;
+                $scope.Data.mode = "showAnswer";
+                $scope.Func.scrollToTop();
+            } else {
+                $scope.Data.showAnswer = true;
+                $("html, body").animate({scrollTop: $('#question-' + answer.num).offset().top }, 500);
             }
-            $scope.Data.mode = "showAnswer";
-            $scope.Func.scrollToTop();
         },
         transferMbtiResult: function () {
             switch ($scope.Data.approvedMbtiGroup) {
@@ -208,16 +265,16 @@ angular.module('detectionModule').controller('detectionCtrl', function ($scope, 
             }
         },
         scrollToTop: function () {
-            // window.scrollTo(0, 0);
-            // window.onbeforeunload = function () {
-            //     window.scrollTo(0, 0);
-            // }
+            window.scrollTo(0, 0);
+            window.onbeforeunload = function () {
+                window.scrollTo(0, 0);
+            }
         },
         calculateFinalResult: function () {
             for (let i = 0; i < 9; i++) {
                 if ($scope.Data.resultGroups[i])
                     $scope.Data.finalResult[i] =
-                        $scope.Data.resultGroups[i].result +
+                        $scope.Data.resultGroups[i] +
                         $scope.Data.probableGroups[i] +
                         $scope.Data.preferredGroups[i] +
                         $scope.Data.transferredMbtiResult[i] +
